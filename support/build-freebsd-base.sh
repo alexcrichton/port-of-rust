527b519765e19261368006512694d2b2a628b08f
Alex Crichton <alex@alexcrichton.com>
2016-03-13 13:06:07 -0700

    Copy in librt/libexecinfo to freebsd image

--- support/build-freebsd-base.sh
+++ support/build-freebsd-base.sh
@@ -14,10 +14,14 @@ cp /tmp/freebsd/usr/lib/crti.o lib
 cp /tmp/freebsd/usr/lib/crtn.o lib
 cp /tmp/freebsd/usr/lib/libc.a lib
 cp /tmp/freebsd/usr/lib/libm.a lib
+cp /tmp/freebsd/usr/lib/librt.so.1 lib
+cp /tmp/freebsd/usr/lib/libexecinfo.so.1 lib
 cp /tmp/freebsd/lib/libc.so.7 lib
 cp /tmp/freebsd/lib/libm.so.5 lib
 cp /tmp/freebsd/lib/libthr.so.3 lib/libpthread.so
 
 ln -s libc.so.7 lib/libc.so
 ln -s libm.so.5 lib/libm.so
+ln -s librt.so.1 lib/librt.so
+ln -s libexecinfo.so.1 lib/libexecinfo.so
 rm -rf /tmp/freebsd
